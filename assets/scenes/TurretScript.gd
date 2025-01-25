369057c9fffdcb8c3ecd9ecd557b6a37d01a3212
LeeDong <qaziop002@gmail.com>
2025-01-25 19:24:21 +0800

    feat: add shoot bubble and rotate audio

--- assets/scenes/TurretScript.gd
+++ assets/scenes/TurretScript.gd
@@ -1,6 +1,10 @@
 extends Sprite2D
 
 @onready var bubbleShot = preload("res://assets/scenes/BubbleShot.tscn")
+
+@export var shoot_player: AudioStreamPlayer2D
+@export var rotate_player: AudioStreamPlayer2D
+
 #var rocketBody : RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,11 +16,14 @@ func _physics_process(delta: float) -> void:
 	#set_rotation(clamp(rad_to_deg(get_rotation()),-180,180))
 	if Input.is_action_pressed("TurretCW"):
 		rotate(rad_to_deg(1)*0.1*delta)
+		rotate_player.play();
 	if Input.is_action_pressed("TurretCCW"):
 		rotate(-rad_to_deg(1)*0.1*delta)
+		rotate_player.play();
 	if Input.is_action_just_pressed("ui_accept"):
 		var shot = bubbleShot.instantiate()
 		$Marker2D.add_child(shot)
+		shoot_player.play()
 		
 		#get_parent().set_freeze_enabled(false)
 		get_parent().apply_impulse(Vector2(0,-10))
