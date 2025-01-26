1811577003ffac33d7a14df9034dc35092151bf0
LeeDong <qaziop002@gmail.com>
2025-01-26 11:19:18 +0800

    temp: all flow

--- src/GameManager.cs
+++ src/GameManager.cs
@@ -1,5 +1,52 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class GameManager : Node
 {
+	[Export] public PackedScene[] levels;
+	[Export] public int currentLevelId = 0;
+
+	[Export] public TitleView titleView;
+
+
+	public override void _Ready()
+	{
+		// register ui event
+		titleView.startBtn.Pressed += StartNewGame;
+		titleView.exitBtn.Pressed += ExitGame;
+
+
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+	}
+
+	public void StartNewGame()
+	{
+		GD.Print("StartNewGame");
+		// reset player
+
+		// reset current level
+		currentLevelId = 0;
+	}
+
+	public void StartNextLevel()
+	{
+		// reset player
+
+		// reset current level
+		currentLevelId++;
+
+	}
+
+	public void ReturnToTitle()
+	{
+
+	}
+
+	public void ExitGame(){
+		GetTree().Quit();
+	}
 }
--- src/View/TitleView.cs
+++ src/View/TitleView.cs
@@ -1,11 +1,15 @@
 using Godot;
 using System;
 
-public partial class TitleView : Node
+public partial class TitleView : Control
 {
+	[Export] public Button startBtn;
+	[Export] public Button exitBtn;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// startBtn.Connect();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
