1e761da1646e1389ec8cd201c7e8c96316d71fed
LeeDong <qaziop002@gmail.com>
2025-01-26 14:16:33 +0800

    fixed: final level detect

--- src/Level/GoalArea.cs
+++ src/Level/GoalArea.cs
@@ -6,17 +6,17 @@ public partial class GoalArea : Area2D
 	[Export] public LevelController controller;
 
 	public override void _Ready()
-    {
-        // 連接 body_entered 信號
-        Connect("body_entered", new Callable(this, "_OnBodyEntered"));
-    }
+	{
+		// 連接 body_entered 信號
+		Connect("body_entered", new Callable(this, "_OnBodyEntered"));
+	}
 
-    private void _OnBodyEntered(Node body)
-    {
-        if (body.Name == "RocketBody") // 檢查是否是玩家
-        {
-            GD.Print("Player entered the area!");
-            controller.HitGoal();
-        }
-    }
+	private void _OnBodyEntered(Node body)
+	{
+		if (body.Name == "RocketBody") // 檢查是否是玩家
+		{
+			GD.Print("Player entered the area!");
+			controller.HitGoal();
+		}
+	}
 }
